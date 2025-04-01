7275f62c0b711fb9a73210837aadaebcf0eef036
Sunil7033 <sunilgithub9162@gmail.com>
2025-04-01 11:08:24 +0530

    Enable lowercase letters in Captcha generation

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final CaptchaController _captchaController = CaptchaController();
+  final CaptchaController _captchaController = CaptchaController(
+    includeLowercase: true,
+  );
+
   Uint8List? _captchaBytes;
   final TextEditingController _textEditingController = TextEditingController();
 
